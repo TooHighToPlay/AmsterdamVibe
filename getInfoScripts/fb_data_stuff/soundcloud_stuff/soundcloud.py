60ef8640fda96ddf83a9006a204175e80fa0c490
Zilvinas Kucinskas <zil.kucinskas@gmail.com>
2014-03-23 18:16:12 +0100

    Import initial template for report

--- getInfoScripts/fb_data_stuff/soundcloud_stuff/soundcloud.py
+++ getInfoScripts/fb_data_stuff/soundcloud_stuff/soundcloud.py
@@ -1,9 +1,13 @@
-import soundcloud
+import urllib2
+import ast
 
 # Read client id from token.txt
 f = open('token.txt', 'r')
 YOUR_CLIENT_ID = f.read()
 f.close()
+
+content = urllib2.urlopen("https://graph.facebook.com/227389920779142?access_token=" + ACCESS_TOKEN + "&fields=cover").read()
+content = ast.literal_eval(content)
 
 client = soundcloud.Client(client_id=YOUR_CLIENT_ID)
 track = client.get('/tracks/30709985')
